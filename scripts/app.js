332af8977ac4ad95acd384c33927d0a6c72724df
sachinrajc <sachinrajc7@gmail.com>
2024-07-10 17:13:53 -0400

    2nd commit

---
+++ scripts/app.js
@@ -0,0 +1,48 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            FirstName: '',
+            LastName: '',
+            quantity: 0,
+            pricePerItem: 10,
+            myCounter: 10
+        };
+    },
+    computed: {
+        fullName() {
+            return `${this.FirstName} ${this.LastName}`;
+        },
+        totalPrice() {
+            return this.quantity * this.pricePerItem;
+        }
+    },
+    methods: {
+        clearFields() {
+            this.FirstName = '';
+            this.LastName = '';
+            this.quantity = 0;
+        },
+        increase() {
+            setTimeout(() => {
+                this.myCounter++;
+                if (this.myCounter === 10) {
+                    this.decrease();
+                } else {
+                    this.increase();
+                }
+            }, 500);
+        },
+        decrease() {
+            setTimeout(() => {
+                this.myCounter--;
+                if (this.myCounter !== 0) {
+                    this.decrease();
+                } else {
+                    this.increase();
+                }
+            }, 500);
+        }
+    }
+});
+
+app.mount('#app');
